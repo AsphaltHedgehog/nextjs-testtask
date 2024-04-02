f9d68138d905f822df11643fc1d43bb04b0d9dba
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-02 23:19:02 +0300

    simplified JobList

--- src/components/jobList/JobList.tsx
+++ src/components/jobList/JobList.tsx
@@ -5,7 +5,6 @@ import JobCard from "@/components/jobCard";
 
 // interface
 import { IJob } from '../fetchJobs'
-import { toast } from 'react-toastify';
 
 export interface JobListProps {
   jobInfoArray: IJob[]
@@ -15,12 +14,6 @@ export interface JobListProps {
 const JobList = ({ jobInfoArray, setFavoriteIds }: JobListProps) => {
   const listCreator = (jobInfoArray: IJob[]) => {
 
-    if (jobInfoArray.length < 1) {
-      toast.error("Internal app error")
-      return;
-    };
-
-    // TODO: add navigation
     const elements = jobInfoArray.map(jobInfo => (
       <li key={jobInfo.job_id} onClick={() => console.log(1)}>
         <JobCard jobInfo={jobInfo} setFavoriteIds={ setFavoriteIds }/>
