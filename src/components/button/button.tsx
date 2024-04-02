cebc3b17067329bc98bb2317a683c96918af9b6c
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-02 19:45:12 +0300

    fixed btn

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,39 +2,41 @@
 
 import React, { ButtonHTMLAttributes, useEffect, useState } from "react";  
 
+
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 // components
 import toggleFavorite from "../toggleFav";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  disabled?: boolean;
-  children?: string;
-  favorite: boolean;
   jobId: string;
+  setFavoriteIds?: React.Dispatch<React.SetStateAction<string[]>>
 }
 
-export default function Button({ disabled, favorite, children, jobId, ...rest }: ButtonProps) {
+export default function Button({ jobId, setFavoriteIds, ...rest }: ButtonProps) {
   const [favArr, setFavArr] = useState<string[]>([]);
 
   useEffect(() => {
-    if (favorite) {
-      const storedTheme = localStorage.getItem("data");
-      if (storedTheme && favArr.length < 1) {
-        setFavArr(JSON.parse(storedTheme));
-      }
-    }
-  }, [favArr.length, favorite]);
+    const storedTheme = localStorage.getItem("data");
+    if (storedTheme && storedTheme.length < 1) {
+      return;
+    };
+    if (storedTheme !== null) {
+      setFavArr(JSON.parse(storedTheme));
+    };
+  }, []);
 
   const handleClick = () => {
-    
-    toggleFavorite({jobId, favArr, setFavArr})
+    toggleFavorite({ jobId, setFavArr, setFavoriteIds })
+    toast.success(favArr.includes(jobId) ? "Successfully removed" : "Successfully added");
   };
 
   return (
     <button {...rest}
       onClick={handleClick}
-      className='py-2.5 px-5 bg-gray-900 text-zinc-50 text-base text-center font-medium rounded'>
-      {/* {jobId} */}
-      {children}
+      className={`py-2.5 px-5 bg-gray-900 text-zinc-50 text-base text-center font-medium rounded' ${favArr.includes(jobId) ? 'bg-pink-600' : ""} favButton`}>
+      {favArr.includes(jobId) ? "Unlike" : "Like"}
     </button>
   );
 };
