8c64e9a0bd5d982e47793a748cd58450f9bf8a50
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-02 19:45:37 +0300

    complete liked page

--- src/app/liked/page.tsx
+++ src/app/liked/page.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+'use client'
 
-type Props = {}
+import React, { useEffect, useState } from "react";
 
-function page({}: Props) {
+// api
+import fetchJobsDetail from "@/api/fetchJobsDetail";
+
+// components
+import { IJob } from "@/components/fetchJobs";
+import JobList from "@/components/jobList";
+
+export default function LikedPage() {
+  const [jobs, setJobs] = useState<IJob[]>([]);
+  const [favoriteIds, setFavoriteIds] = useState<string[]>([]);
+  
+  useEffect(() => {
+    const storedJobs = localStorage.getItem("data");
+    if (!storedJobs || storedJobs.length < 1) {
+      return
+    };
+      
+    setFavoriteIds(JSON.parse(storedJobs));
+  }, [])
+
+  useEffect(() => {
+    fetchJobsDetail({ jobsId: favoriteIds, setJobs })
+  }, [favoriteIds]);
+
   return (
-    <div>page</div>
+    <main>
+      <h2>Liked Jobs</h2>
+      <section>
+        <> <JobList jobInfoArray={jobs} setFavoriteIds={ setFavoriteIds  } /> </>
+      </section>
+    </main>
   )
 }
-
-export default page
