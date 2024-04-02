e1b5d938740206e112ccbdbc1af23d341bb21b6d
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-03 02:26:09 +0300

    complete jobsPage

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -3,28 +3,65 @@
 import React, { useEffect, useState } from "react";
 
 // components
-import { IJob } from "@/api/fetchJobsTitle";
+import fetchJobsTitle, { IJob } from "@/api/fetchJobsTitle";
 import JobList from "@/components/jobList";
+import { IUserFormValues } from "@/components/form";
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
+
+
+// schema
+import searchValidationSchema from "@/schemas/searchSchema";
+
+
 
 export default function JobsPage() {
-  const [jobs, setJobs] = useState<IJob[]>([])
+  const [jobs, setJobs] = useState<IJob[]>([]);
+  let count = 0
+
+  const userString = typeof window !== 'undefined' ? localStorage.getItem('user') : null;
   
+  const user: IUserFormValues | null = userString ? JSON.parse(userString) : null;
+
+  const initialValues = {
+  search: user? user.job : '' ,
+};
+
   useEffect(() => {
-    if (jobs.length < 1) {
-      // fetchJobs({setJobs});
+    if (jobs.length < 1 && user && count === 0) {
+      count = 1;
+      fetchJobsTitle({ jobTitle: user.job, setJobs });
     }
-  }, [jobs.length])
+
+    
+  }, [jobs.length, user])
+
+  const handleSubmit = (values: { search: string }, formikHelpers: FormikHelpers<{ search: string }>) => {
+    fetchJobsTitle({ jobTitle: values.search, setJobs });
+  };
 
   return (
     <main>
-      <section>
-        <h1>Find your Dream Job</h1>
-        <div>Filter</div>
-        {jobs.length > 0 ?
-          <div>Jobs: <JobList jobInfoArray={ jobs } /> </div> :
-          <p>Loading...</p>
-        }
-      </section>
+      <>
+        <h2>Find your Dream Job</h2>
+        <section>
+          <h2>Filter</h2>
+          <Formik onSubmit={handleSubmit} initialValues={initialValues} validationSchema={searchValidationSchema}>
+            <Form>
+              <label>Search
+                <Field type='text' name='search'></Field>
+              </label>
+              <ErrorMessage name="search" component='div' />
+              <button type='submit'>Submit</button>
+            </Form>
+          </Formik>
+        </section>
+
+      
+          <section>
+          {jobs.length > 0 ? <><h2>Jobs:</h2> <JobList jobInfoArray={ jobs } /> </> : <p>Loading...</p>}
+          </section>
+        
+      </>
     </main>
   );
-}
+};
