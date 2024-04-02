665a778927795977e241eaa3705c3fd8c46ff1ef
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-03 00:04:43 +0300

    added env

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['upload.wikimedia.org', "encrypted-tbn0.gstatic.com", "firstteeaustin.org"],
   },
+  env: {
+    apiKey: 'f63cf3ec6emsh3d725b5c79a169bp1dd2fcjsn36ea03d0676c',
+  },
 };
 
 export default nextConfig;
