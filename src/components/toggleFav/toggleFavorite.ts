70ee6d6fd9a3fb990be60d09d5b5deb943084783
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-02 19:40:53 +0300

    fixed toggle favorite func

--- src/components/toggleFav/toggleFavorite.ts
+++ src/components/toggleFav/toggleFavorite.ts
@@ -1,29 +1,39 @@
+import { toast } from "react-toastify";
+
 export interface ToggleFavoriteProps {
   jobId: string;
-  favArr: string[];
   setFavArr: React.Dispatch<React.SetStateAction<string[]>>
+  setFavoriteIds?: React.Dispatch<React.SetStateAction<string[]>>
 }
 
-const toggleFavorite = ({ jobId, favArr, setFavArr }: ToggleFavoriteProps) => {
-  if (!jobId && favArr.length < 0) {
+const toggleFavorite = async ({ jobId, setFavArr, setFavoriteIds }: ToggleFavoriteProps) => {
+  if (!jobId) {
+    toast.error('internal app error')
     return
-    // add toast for error
-  }
-    
-  let favoriteArrayCopy = [...favArr];
+  };
+  
+  
+    let favoriteArrayCopy = []
+    const storageData = await localStorage.getItem("data");
+    if (storageData !== null) {
+      favoriteArrayCopy = JSON.parse(storageData);
+    }
 
   if (favoriteArrayCopy.includes(jobId)) {
     const index = favoriteArrayCopy.indexOf(jobId);
-
     if (index !== -1) {
       favoriteArrayCopy.splice(index, 1);
-    }
+    };
   } else {
     favoriteArrayCopy.push(jobId)
-  }
+  };
 
-  setFavArr(favoriteArrayCopy)
+  if (setFavoriteIds) {
+    setFavoriteIds(favoriteArrayCopy)
+  };
+
+  setFavArr(favoriteArrayCopy);
   localStorage.setItem("data", JSON.stringify(favoriteArrayCopy));
 };
 
-export default toggleFavorite
+export default toggleFavorite;
