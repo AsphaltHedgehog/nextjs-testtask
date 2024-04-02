408dac17f4c4b4031a963d66c98388920d9784de
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-02 19:46:43 +0300

    start work on createProfile page

---
+++ src/app/create-profile/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+export default function UserProfile() {
+  return (
+    <main>
+      <section>
+        <h1>Find your AAAA Job</h1>
+      </section>
+    </main>
+  );
+}
+
