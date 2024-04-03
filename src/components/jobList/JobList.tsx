ae997a82558c9625f289ff8e8593128519659a8b
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-03 07:26:32 +0300

    updated jobList styles

--- src/components/jobList/JobList.tsx
+++ src/components/jobList/JobList.tsx
@@ -14,8 +14,8 @@ const JobList = ({ jobInfoArray, setFavoriteIds }: JobListProps) => {
   const listCreator = (jobInfoArray: IJob[]) => {
 
     const elements = jobInfoArray.map(jobInfo => (
-      <li key={jobInfo.job_id}>
-        <JobCard jobInfo={jobInfo} setFavoriteIds={ setFavoriteIds }/>
+      <li key={jobInfo.job_id} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 px-2 h-72">
+          <JobCard jobInfo={jobInfo} setFavoriteIds={setFavoriteIds} />
       </li>
     ))
 
@@ -23,7 +23,7 @@ const JobList = ({ jobInfoArray, setFavoriteIds }: JobListProps) => {
   };
   
   return (
-    <ul>
+    <ul className="flex flex-wrap justify-center">
       {listCreator(jobInfoArray)}
     </ul>
   )
