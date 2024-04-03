e732cf8ebddd1cbbbc9b147302bc2a113e4e7058
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-03 07:43:57 +0300

    fixed style for JobList

--- src/components/jobList/JobList.tsx
+++ src/components/jobList/JobList.tsx
@@ -14,7 +14,7 @@ const JobList = ({ jobInfoArray, setFavoriteIds }: JobListProps) => {
   const listCreator = (jobInfoArray: IJob[]) => {
 
     const elements = jobInfoArray.map(jobInfo => (
-      <li key={jobInfo.job_id} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 px-2 h-72">
+      <li key={jobInfo.job_id} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/3 mb-4 px-2 h-72">
           <JobCard jobInfo={jobInfo} setFavoriteIds={setFavoriteIds} />
       </li>
     ))
