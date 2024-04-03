f3df10aec37285ab194c1ab1cffcb8037926db14
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-03 07:20:08 +0300

    update apikey

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     // domains: ['upload.wikimedia.org', "encrypted-tbn0.gstatic.com", "firstteeaustin.org", "www.teksystems.com", "image.status.io", "prod-file-upload-store.s3.amazonaws.com", "pbs.twimg.com", "www.icf.com", "www.mastechdigital.com", "www.unum.com", "app-data.gcs.trstatic.net"],
   },
   env: {
-    apiKey: 'a9681e5d77mshe54731460894278p138f6ajsn3e29791c6eab',
+    apiKey: 'cd50da44ccmsh858b194db58a735p1d4dedjsnccafd133f371',
   },
 };
 
