b8261e0f95043c1d53fcc70b078c6ad49eada358
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-04-03 07:25:58 +0300

    updated header styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,27 +6,29 @@ export interface HeaderProps {
 
 const Header = ({}: HeaderProps) => {
   return (
-    <header>
-      <nav>
-        <Link href="/">
-          JobFinder
-        </Link>
-
-        <Link href='/jobs'>
+    <header className="bg-blue-500 mb-10">
+      <nav className="flex items-center justify-between max-w-4xl p-4 mx-auto">
+        <div className="text-white">
+          <Link href="/" className="text-xl font-bold">
+            JobFinder
+          </Link>
+        </div>
+        <div className="flex gap-8">
+        <Link href='/jobs' className="hover:text-gray-300">
           Jobs
         </Link>
 
-        <Link href="/create-profile">
+        <Link href="/create-profile" className="hover:text-gray-300" >
           My Profile
         </Link>
 
-        <Link href="/liked">
+        <Link href="/liked" className="hover:text-gray-300">
           Liked Jobs
         </Link>
-
+        </div>
       </nav>
     </header>
   )
-}
+};
 
 export default Header
